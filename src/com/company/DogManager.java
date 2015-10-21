1505281fab7453a0213df358d922e14b0ba352de
Clara <minneapolis-edu@users.noreply.github.com>
2015-10-21 15:15:29 -0500

    Update DogManager.java

--- src/com/company/DogManager.java
+++ src/com/company/DogManager.java
@@ -5,6 +5,14 @@ public class DogManager {
     public static void main(String[] args) {
 
 
+        //Day numbers - day names hashmap
+        /*
+        HashMaps<Integer, String> dayLookup = new HashMap();
+        dayLookup.put(0, "Monday");
+        dayLookup.put(1, "Tuesday");
+        //etc...
+        */
+        
         //TODO store all of the dogs in a list, for example an ArrayList
         //TODO Format output as specified in the lab
 
@@ -25,7 +33,8 @@ public class DogManager {
             //TODO instead of working with each Dog individually, get dog information from your list of Dog objects.
             //So you'll need another loop here, to loop over te Dogs list
             System.out.println(testDog.getName());
-            System.out.println("On day " + day + " walk dog this many times "+ testDog.getNumberOfWalksForDay(day));
+            //String nameOfDay = dayLookup.get(day);   ///get name of day from HashMap.
+            System.out.println("On day " + nameOfDay + " walk dog this many times "+ testDog.getNumberOfWalksForDay(day));
 
 
         }
